b781aa1d44260ed05a7c23defb92134bc788dc9f
griffnb <griffnb@gmail.com>
2024-01-19 08:54:41 -0700

    v2 types

--- aws.go
+++ aws.go
@@ -3,9 +3,10 @@ package decimal
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
+/* AWS v1
 func (this *Decimal) UnmarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) error {
 	if av.N == nil {
 		return nil
@@ -22,4 +23,23 @@ func (this *Decimal) MarshalDynamoDBAttributeValue(av *dynamodb.AttributeValue) 
 	n := fmt.Sprintf("%s", this)
 	av.N = &n
 	return nil
+}
+*/
+//AWS V2
+func (this *Decimal) UnmarshalDynamoDBAttributeValue(av types.AttributeValue) error {
+	avN, ok := av.(*types.AttributeValueMemberN)
+	if !ok {
+		return fmt.Errorf("expected AttributeValueMemberN, got %T", av)
+	}
+	var err error
+	*this, err = NewFromString(avN.Value)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *Decimal) MarshalDynamoDBAttributeValue() (types.AttributeValue, error) {
+	n := fmt.Sprintf("%s", this)
+	return &types.AttributeValueMemberN{Value: n}, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/shopspring/decimal
 
 go 1.13
 
-require github.com/aws/aws-sdk-go v1.48.7
+require github.com/aws/aws-sdk-go-v2/service/dynamodb v1.26.9
